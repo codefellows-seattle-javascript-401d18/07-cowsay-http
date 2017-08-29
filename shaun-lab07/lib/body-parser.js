387d0793dbd24a05c9fbc4f3f99e6556eeaeb1d1
Shaun Wahle <shaun.wahle@gmail.com>
2017-08-29 13:26:10 -0700

    Modulizing body-parser

---
+++ shaun-lab07/lib/body-parser.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.export = {};
+
+const bodyParse = (req, callback) => {
+  if (req.method === 'POST' || req.method === 'PUT') {
+    let body = '';
+    req.on('data', (buf) => {
+      body += buf.toString();
+    });
+    req.on('end', () => callback(null, body));
+    req.on('error', (err) => callback(err));
+  } else {
+    callback(null, '{}');
+  }
+};
