a21d5ccf64b9557068e563b8c6efc1a3f46850dc
Aaron McPeek <arn1313@comcast.net>
2017-08-30 09:32:01 -0700

    added in body-parser.js

--- lab-aaron/server.js
+++ lab-aaron/server.js
@@ -1,25 +1,27 @@
 'use strict';
 //require in dependencies
 const http = require('http');
-const url = require('url');
-const querystring = require('querystring');
+const url = require('url').parse;
+const querystring = require('querystring').parse;
 const cowsay = require('cowsay');
+const bodyParse = require('/lib/body-parse');
 const PORT = process.env.PORT || 3000;
 
 //create body parser modeling lab example
-const bodyParse = (req, callback) => {
-  if (req.method === 'POST' || req.method === 'PUT') {
-    let body = '';
-    req.on('data', (buf) => {
-      body += buf.toString();
-    });
-    req.on('end', () => callback(null, body));
-    req.on('error', (err) => callback(err));
-  } else {
-    callback(null, '{}');
-  }
-}
+// const bodyParse = (req, callback) => {
+//   if (req.method === 'POST' || req.method === 'PUT') {
+//     let body = '';
+//     req.on('data', (buf) => {
+//       body += buf.toString();
+//     });
+//     req.on('end', () => callback(null, body));
+//     req.on('error', (err) => callback(err));
+//   } else {
+//     callback(null, '{}');
+//   }
+// }
 
+cowsay.say({ text: 'Some message for cow to say' })
 //create the server modeling the lab example
 const server = module.exports = http.createServer((req, res) =>{
   req.url = url.parse(req.url);
@@ -43,12 +45,15 @@ const server = module.exports = http.createServer((req, res) =>{
       res.writeHead(200, {
         'Content-Type': 'text/plain'
       })
-      res.write
+      res.write('Hello from my server');
       res.end();
       return;
     }
   }
 
+  server.listen(3000, () => {
+    console.log('Listening on port: 3000')
+  });
   //for ALL requests made to /, the server should respond with the following:
   //a header containing Content-Type: text/plain
   //a status code of 200
@@ -77,12 +82,3 @@ const server = module.exports = http.createServer((req, res) =>{
   // if the JSON {text: messsage} is not set in the body, respond with:
   // status code 400
   // a body including the value returned from cowsay.say({ text: 'bad request' })
-
-
-
-
-
-
-  server.listen(3000, () => {
-    console.log('Listening on port: 3000')
-  });
