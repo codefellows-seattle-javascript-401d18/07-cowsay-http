4e86bcc825c811b5487bc3e7b95b8cc6498fba76
Michelle Scharlock <michelle.scharlock@gmail.com>
2017-09-02 17:32:25 -0700

    slight adjust to bodyparser

--- lab-michelle/module/bodyParse.js
+++ lab-michelle/module/bodyParse.js
@@ -1,13 +1,6 @@
-//Ok I don't think this is really customized but it is in a separate module. Does that count?
 'use strict';
 
-//have all the things, bodyParser
-const http = require('http');
-const url = require('url');
-// const querystring = require('querystring');
-// const cowsay = require('cowsay');
-
-const bodyParse = (req, callback) => {
+module.exports = (req, callback) => {
   if (req.method === 'POST' || req.method === 'PUT') {
     let body = '';
     req.on('data', (buf) => {
