c6aae75e253ec7f9951f4a703cd1a4ea3ea90849
Michelle Scharlock <michelle.scharlock@gmail.com>
2017-09-02 17:30:17 -0700

    Fixed the server ala Scott's code

--- lab-michelle/server.js
+++ lab-michelle/server.js
@@ -1,16 +1,17 @@
 'use strict';
 
 const http = require('http');
-const url = require('url');
-const querystring = require('querystring');
+const url = require('url').parse;
+const querystring = require('querystring').parse;
 const cowsay = require('cowsay');
 const bodyParser = require('../module/bodyParse.js');
 // const PORT = 3000 || process.env.PORT;
 
 
 const server = module.exports = http.createServer((req, res) => {
-  req.url = url.parse(req.url);
-  req.url.query = querystring.parse(req.url.query);
+  req.url = url(req.url);
+  req.url.query = querystring(req.url.query);
+
   bodyParser(req, (err, body) => {
     if (err) {
       // resond to the user with a 500 server error
@@ -18,12 +19,14 @@ const server = module.exports = http.createServer((req, res) => {
       res.end();
       return;
     }
+
     // parse the body as json
     try {
       req.body = JSON.parse(body);
     } catch (err) {
       // if there was mal formated JSON we send back a 400 bad request
-      res.writeHead(400);
+      res.writeHead(400, {'Content-Type': 'text/plain'});
+      res.write('bad request: no body provided');
       res.end();
       return;
     }
@@ -33,44 +36,42 @@ const server = module.exports = http.createServer((req, res) => {
     //we don't specify get, put, etc and then it should apply to all
     if (req.url.pathname === '/') {
       res.writeHead(200, {
-        'Content-Type': 'application/json',
+        'Content-Type': 'text/plain',
       });
-      res.write(JSON.stringify('hello from my server'));
+      res.write('hello from my server');
       res.end();
       return;
     }
 
-    //so this is super annoying but apparently it is broken, i think it is because we need somewhere to have people specify what req.url.query.text is??
     if (req.method === 'GET' && req.url.pathname === '/cowsay') {
-      if (!req.url.query.text) {
-        res.writeHead(400);
-        res.writeHead(cowsay.say({text: 'bad request', e: 'oO', T: 'U ', f:'meow.cow' }));
-        res.end();
-      } else {
-        res.writeHead(200, {
-          'Content-Type': 'application/json',
-        });
-        res.writeHead(cowsay.say({text: req.url.query.text, e: 'oO', T: 'U ', f:'whale.cow' }));
-        res.end();
-        return;
-      }
-    }
-  });
-
-  if (req.method === 'POST' && req.url.pathname === '/cowsay') {
-    if (!req.url.query.text) {
-      res.writeHead(400);
-      res.writeHead(cowsay.say({ text: 'bad request' , e: 'oO', T: 'U', f: 'dragon.cow'}));
-      res.end();
-    } else {
-      res.writeHead(200, {
-        'Content-Type': 'application/json',
-      });
-      res.write(cowsay.say(req.url.query));
+      res.writeHead(400, {'Content-Type': 'text/plain'});
+      let cow = cowsay.say(
+        {
+          text: req.url.query.text,
+          f: req.url.query.f,
+        }
+      );
+      res.write(cow);
       res.end();
       return;
     }
-  }
+
+    if (req.method === 'POST' && req.url.pathname === '/cowsay') {
+      res.writeHead(200, {
+        'Content-Type': 'text/plain',
+      });
+      let cow = cowsay.say({text: req.body.text});
+      res.write(cow);
+      res.end();
+      return;
+    }
+    //the otherwise 400 - instead of writing in 400 for every one like I did, we do kind of a giant if/else //
+
+    res.writeHead(400, {'Content-Type': 'text/plain'});
+    let cow = cowsay.say({text: 'bad route, mooooo!'});
+    res.write(cow);
+    res.end();
+  });
 });
 
 server.listen(3000, () => {
