ad0c415bd4a1b44ea40901dadc3f512a9a95233a
kpryzk <kepryzk@gmail.com>
2017-09-10 17:10:33 -0700

    init commit

--- lab-katherine/lib/body-parser.js
+++ lab-katherine/lib/body-parser.js
@@ -1,8 +1,13 @@
+'use strict'
+
 module.exports = (req, callback) => {
   if (req.method === 'POST' || req.method === 'PUT') {
     let body = ''
-    req.on('data', buff => body +=buff.toString())
+    req.on('data', buff => body += buff.toString())
     req.on('end', () => callback(null,body))
-    req.on('error'...........)
-  }
+    req.on('error', err => {
+      console.error(err)
+      callback(err)
+    })
+  } else callback(null, '{}')
 }
